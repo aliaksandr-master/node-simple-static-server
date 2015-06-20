9ae64ae9d87f8df94537b1e1362f0bfe0ab66ce9
ALiaksandr Pasynkau <alxe.master@gmail.com>
2015-06-20 15:02:43 +0300

    add check process

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = require('grunto')(function (grunt) {
+
+	grunt.registerTask('test', [
+		'eslint:lib',
+		'eslint:other',
+		'nodeunit'
+	]);
+
+	return {
+		eslint: {
+			lib: [
+				'lib/**/*.js'
+			],
+			other: [
+				'**/*.js',
+				'!lib/**/*.js',
+				'!node_modules/**/*',
+				'!lib-cov/**/*'
+			]
+		},
+		nodeunit: {
+			tests: [
+				'tests/*.js'
+			]
+		}
+	};
+}); 
